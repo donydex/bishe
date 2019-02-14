2aa966db59f8d0feacd2f36fefaa0cfc578a4866
donydex <donydex@163.com>
2019-02-14 16:35:30 +0800

    docker_output ok json_grade has problems

--- calculate_grade_csv.py
+++ calculate_grade_csv.py
@@ -1,6 +1,7 @@
 import pandas
 import numpy
 import sys
+import os
 
 
 class calculate_grade_csv(object):
@@ -8,6 +9,7 @@ class calculate_grade_csv(object):
         file_name = sys.argv[1]
         self.docker_file = "docker_output_{0}.csv".format(file_name)
         self.json_file = "json_data_{0}.csv".format(file_name)
+        self.grade_file = "grade_data_{0}.csv".format(file_name)
         self.api_name = sys.argv[2]
 
     def calculate_docker_grade(self):
@@ -22,9 +24,21 @@ class calculate_grade_csv(object):
         json_grade = numpy.mean(data["duration"])
         return json_grade
 
+    def create_csv(self):
+        if not os.path.isfile(self.grade_file):
+          df = pandas.DataFrame(columns=["api_name", "docker_grade", "json_grade"])
+          df.to_csv(self.grade_file, index=False)
+
+    def store_grade(self, docker_grade, json_grade):
+        df = pandas.read_csv(self.grade_file)
+        df.loc[df.shape[0]] = {'api_name':self.api_name, 'docker_grade':docker_grade, 'json_grade':json_grade}
+        df.to_csv(self.grade_file, index=False)
+
     def main(self):
         print(self.calculate_docker_grade())
         print(self.calculate_json_grade())
+        self.create_csv()
+        self.store_grade(self.calculate_docker_grade(), self.calculate_json_grade())
         return self.calculate_docker_grade(), self.calculate_json_grade()
 
 
